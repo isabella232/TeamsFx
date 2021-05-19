d2b2be8b1fc2a1fe396a0db23af494803de7a293
huajiezhang <huajiezhang@microsoft.com>
2021-05-20 00:31:09 +0800

    up

--- packages/api/src/core.ts
+++ packages/api/src/core.ts
@@ -1,76 +1,109 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-"use strict";
 
-/*
- *  ------------
- * |  extension | -- UI & dialog & telemetry & logger
- *  ------------
- *
- *  ------------
- * |    core    | -- Environments & Project
- *  ------------
- *
- *  ------------------
- * |  solution plugin | -- General lifecycle
- *  -------------------
- *
- *  ----------------------
- * |   resource plugin   | -- Specific lifecycle
- *  ----------------------
- */
- 
-import {  Result } from "neverthrow";  
-import { FunctionRouter, FxError,  ProjectConfigs,  QTreeNode, Task, Void, Func, Inputs, UserInterface} from "./index";
-
-
-
+import { Result } from "neverthrow";
+import {
+  FunctionRouter,
+  FxError,
+  ProjectConfigs,
+  QTreeNode,
+  Task,
+  Void,
+  Func,
+  Inputs,
+  UserInterface,
+} from "./index";
 
 export interface Core {
+  init: (systemInputs: Inputs) => Promise<Result<Void, FxError>>;
 
-    init: (systemInputs: Inputs) => Promise<Result<Void, FxError>>;
+  /**
+   * create a project, return the project path
+   * Core module will not open the created project, extension will do this
+   */
+  createProject: (
+    systemInputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<string, FxError>>;
+  /**
+   * provision resource to cloud
+   */
+  provisionResources: (
+    systemInputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<Void, FxError>>;
 
-    /**
-     * create a project, return the project path
-     * Core module will not open the created project, extension will do this
-     */
-    createProject: (systemInputs: Inputs, ui: UserInterface) => Promise<Result<string, FxError>>;
-    /**
-     * provision resource to cloud
-     */
-    provisionResources: (systemInputs: Inputs, ui: UserInterface) => Promise<Result<Void, FxError>>;
+  /**
+   * build artifacts
+   */
+  buildArtifacts: (
+    systemInputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<Void, FxError>>;
 
-    /**
-     * build artifacts
-     */
-    buildArtifacts: (systemInputs: Inputs, ui: UserInterface) => Promise<Result<Void, FxError>>;
+  /**
+   * deploy resource to cloud
+   */
+  deployArtifacts: (
+    systemInputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<Void, FxError>>;
 
-    /**
-     * deploy resource to cloud
-     */
-    deployArtifacts: (systemInputs: Inputs, ui: UserInterface) => Promise<Result<Void, FxError>>;
+  /**
+   * publish app
+   */
+  publishApplication: (
+    systemInputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<Void, FxError>>;
 
-    /**
-     * publish app
-     */
-    publishApplication: (systemInputs: Inputs, ui: UserInterface) => Promise<Result<Void, FxError>>;
+  /**
+   * create an environment
+   */
+  createEnv: (
+    systemInputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<Void, FxError>>;
 
-    /**
-     * create an environment
-     */
-    createEnv: (systemInputs: Inputs, ui: UserInterface) => Promise<Result<Void, FxError>>;
+  /**
+   * remove an environment
+   */
+  removeEnv: (
+    systemInputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<Void, FxError>>;
 
-    /**
-     * remove an environment
-     */
-    removeEnv: (systemInputs: Inputs, ui: UserInterface) => Promise<Result<Void, FxError>>;
+  /**
+   * switch environment
+   */
+  switchEnv: (
+    systemInputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<Void, FxError>>;
 
-    /**
-     * switch environment
-     */
-    switchEnv: (systemInputs: Inputs, ui: UserInterface) => Promise<Result<Void, FxError>>;
- 
-    executeUserTask: (func: Func, inputs: Inputs, ui: UserInterface) => Promise<Result<unknown, FxError>>;
-     
-    getProjectConfigs:(systemInputs: Inputs) => Promise<Result<ProjectConfigs, FxError>>; 
+  executeUserTask: (
+    func: Func,
+    inputs: Inputs,
+    ui: UserInterface
+  ) => Promise<Result<unknown, FxError>>;
+
+  getProjectConfigs: (
+    systemInputs: Inputs
+  ) => Promise<Result<ProjectConfigs, FxError>>;
+
+  /**
+   * get question model for lifecycle {@link Task} (create, provision, deploy, publish), Questions are organized as a tree. Please check {@link QTreeNode}.
+   */
+  getQuestionsForLifecycleTask: (
+    task: Task,
+    inputs: Inputs
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
+
+  /**
+   * get question model for plugin customized {@link Task}, Questions are organized as a tree. Please check {@link QTreeNode}.
+   */
+  getQuestionsForUserTask?: (
+    router: FunctionRouter,
+    inputs: Inputs
+  ) => Promise<Result<QTreeNode | undefined, FxError>>;
 }
